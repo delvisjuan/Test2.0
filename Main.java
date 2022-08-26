5496d57b2d73f49c9da6eb6a659f59019e05521a
delvisjuan <100110634+delvisjuan@users.noreply.github.com>
2022-08-25 22:14:14 -0400

    Se añade la posibilidad de imprimir un triangulo

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main{
     public static void main(String[] args) {
+        int rows = 4;
         
+        for(int j=1;j <= rows;j++){
+            for(int i=1;i <= rows-j;i++){
+                System.out.print("  ");
+            }
+            
+            for(int k=1;k<=j; k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
     }
 }
